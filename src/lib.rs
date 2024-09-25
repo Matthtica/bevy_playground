e548b8464b7db1d501e7508fabc3ee2a8b2bfe48
Matthtica <zxnightstalker199733@gmail.com>
2024-09-25 19:17:13 +0630

    main setup

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod demo;
+pub mod utils;
+pub mod snake_game;
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use bevy::{
+    prelude::*,
+    sprite:: Wireframe2dPlugin,
+    window::{PresentMode, Window, WindowTheme, WindowPlugin},
+};
+
+use bevyfun::{
+    fps,
+    demo::MovingCirclePlugin,
+};
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins((DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "Bevy fun".into(),
+            name: Some("bevy_fun_app".into()),
+            present_mode: PresentMode::AutoVsync,
+            fit_canvas_to_parent: true,
+            prevent_default_event_handling: false,
+            window_theme: Some(WindowTheme::Light),
+            ..default()
+        }),
+        ..default()
+    }), Wireframe2dPlugin))
+        .add_plugins(fps::FpsPlugin)
+        .add_plugins(MovingCirclePlugin)
+        .run();
+}
